f5a7b9599011d2488ab28f3a576271e82cc20ac7
cdanielzt <cdanielzt@gmail.com>
2021-11-14 16:42:34 -0600

    group routes middleware

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,23 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('clientes', 'App\Http\Controllers\ClienteController');
+    Route::resource('cursos', 'App\Http\Controllers\CursoController');
+    Route::resource('asistenciaCursos', 'App\Http\Controllers\AsistenciaCursoController');
+    Route::get('reconocimientos/{codigo}/download', 'App\Http\Controllers\ReconocimientoController@download');
+    Route::resource('reconocimientos', 'App\Http\Controllers\ReconocimientoController');
+    Route::resource('designs', 'App\Http\Controllers\DiseñoController');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
-Route::resource('clientes', 'App\Http\Controllers\ClienteController'); 
-Route::resource('cursos', 'App\Http\Controllers\CursoController'); 
-Route::resource('asistenciaCursos', 'App\Http\Controllers\AsistenciaCursoController'); 
-Route::get('reconocimientos/{codigo}/download', 'App\Http\Controllers\ReconocimientoController@download');
-Route::resource('reconocimientos', 'App\Http\Controllers\ReconocimientoController'); 
-Route::resource('designs', 'App\Http\Controllers\DiseñoController'); 
 
 
 Route::get('/prueba', function () {
     return view('pdf.center');
 });
 
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
 
 
